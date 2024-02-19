77ae492e70e725e2a8bd0ec8881cd02cf0935210
SSNgwenya <sunnyngwenya@gmail.com>
2024-02-19 08:03:10 -0800

    functions1.c

--- functions1.c
+++ functions1.c
@@ -130,31 +130,30 @@ int print_hexa_upper(va_list types, char buffer[],
 int print_hexa(va_list types, char map_to[], char buffer[],
 	int flags, char flag_ch, int width, int precision, int size)
 {
-	int i = BUFF_SIZE - 2;
+	int a = BUFF_SIZE - 2;
 	unsigned long int num = va_arg(types, unsigned long int);
-	unsigned long int init_num = num;
+	unsigned long int init_num = x;
 
 	UNUSED(width);
 
-	num = convert_size_unsgnd(num, size);
+	x = convert_size_unsgnd(x, size);
 
-	if (num == 0)
-		buffer[i--] = '0';
+	if (x == 0)
+		buffer[a--] = '0';
 	buffer[BUFF_SIZE - 1] = '\0';
 
-	while (num > 0)
+	while (x > 0)
 	{
-		buffer[i--] = map_to[num % 16];
-		num /= 16;
+		buffer[a--] = map_to[x % 16];
+		x /= 16;
 	}
 
 	if (flags & F_HASH && init_num != 0)
 	{
-		buffer[i--] = flag_ch;
-		buffer[i--] = '0';
+		buffer[a--] = flag_ch;
+		buffer[a--] = '0';
 	}
 
-	i++;
+	a++;
 
-	return (write_unsgnd(0, i, buffer, flags, width, precision, size));
-}
+	return (write_unsgnd(0, a, buffer, flags, width, precision, size));
